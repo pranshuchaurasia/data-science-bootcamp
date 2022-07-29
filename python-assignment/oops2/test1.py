ec9c0c9eca8637653b050e835451feec9dc2a120
Pranshu Chaurasia <pranshuorian001@gmail.com>
2022-07-30 00:00:01 +0530

    Update test1.py

--- python-assignment/oops2/test1.py
+++ python-assignment/oops2/test1.py
@@ -1,8 +1,11 @@
 class Person:
     def __init__(self,name,surname,yob):
         self.name1=name
-        self.surname=surname
+        self.__surname1=surname
         self.yob1=yob
+# __ (double underscore) means private
+# - (single underscore) means protected
 
-
-sudh=Person("pranshu","chaurasia", 1990)
+pran=Person("pranshu","chaurasia", 1990)
+print(pran.name1)
+print(pran._Person__surname1)
