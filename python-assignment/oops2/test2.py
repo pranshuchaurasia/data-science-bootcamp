fd27ce33c56c400acb6c8ef2b313f5c940241d91
Pranshu Chaurasia <pranshuorian001@gmail.com>
2022-07-30 13:04:50 +0530

    Update test2.py

--- python-assignment/oops2/test2.py
+++ python-assignment/oops2/test2.py
@@ -3,17 +3,25 @@ class Person:
     __surname="chaurasia"
     yob=1998
 
-obj = Person()
-print(obj)
+    def _age(self,current_year):
+        return current_year-self.yob
+    def __age1(self,current_year):
+        return current_year-self.yob
 
-#inheritance
+obj = Person()
+print(obj._age(2022))
+print(obj._Person__age1(2022))
 
 class Employee(Person):
-      pass
+      _name = "PC"
+      __surname = "Unknown"
+      yob=1992
 
 obj1=Employee()
 print(obj1)
+print(obj1._age(2022))
 print(obj1.yob)
 print(obj1._name)
 #systerm creates a key value pair for private keyword
 print(obj1._Person__surname)
+print(obj1._Employee__surname)
