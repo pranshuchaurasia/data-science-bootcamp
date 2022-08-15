52f31dccf8ac84ebe6455e25d1ba70e2045edc82
Pranshu Chaurasia <pranshuorian001@gmail.com>
2022-08-15 20:50:27 +0530

    oops 3 abstraction continued

---
+++ python-assignment/oops_class3/test4.py
@@ -0,0 +1,10 @@
+class ineuron:
+    __studens ="data science"
+
+    def students(self):
+        print("Print the class of students", ineuron.__studens)
+
+i=ineuron()
+i.students()
+
+print(i._ineuron__studens)
