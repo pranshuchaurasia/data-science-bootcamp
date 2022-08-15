fc3d0f7d48da607a51f779420ffc14d78ee11062
Pranshu Chaurasia <pranshuorian001@gmail.com>
2022-08-15 20:08:00 +0530

    oops 3 continued

--- python-assignment/oops_class3/main.py
+++ python-assignment/oops_class3/main.py
@@ -1,10 +1,19 @@
-
 class car:
 
     def __init__(self, body, enign, tyre):
-        self.body=body
-        self.engin=enign
-        self.tyre=tyre
+        self.body = body
+        self.engin = enign
+        self.tyre = tyre
 
-    def milage (self):
-        
+    def milage(self):
+        print("milage of this car")
+
+
+class tata(car):
+    pass
+
+
+c = car("solid", "v6", "radial")
+
+t = tata("solid1", "v7", "radial1")
+print(t.milage())
---
+++ python-assignment/oops_class3/test1.py
@@ -0,0 +1,15 @@
+
+class bank:
+    def transaction(self):
+        print("Total transaction value")
+    def account_opening(self):
+        print("This will show account opening status")
+    def deposite(self):
+        print("This will show deposit account")
+
+class HDFC_bank(bank):
+    def hdfc_to_icici(self):
+        print("It will show transaction from hdfc to icici bank ")
+
+class icici(HDFC_bank):
+    pass
