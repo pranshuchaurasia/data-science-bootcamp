8759783c5d06eb34d771f75de202acbed1d75bb2
Pranshu Chaurasia <pranshuorian001@gmail.com>
2022-08-16 11:53:28 +0530

    oops 3 polymorphism python

---
+++ python-assignment/oops_class3/test6.py
@@ -0,0 +1,16 @@
+class ineuron:
+    def student(self):
+        print("The details of all students")
+
+
+class class_type:
+    def class_for_students(self):
+        print("Print the class for student")
+
+
+def test(a, b):
+    return a+b
+
+
+print(test(3, 5))
+print(test("flop ", "movie"))
