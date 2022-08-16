04b62aba8cc7fe9c8c301dc6637662e41c01b437
Pranshu Chaurasia <pranshuorian001@gmail.com>
2022-08-16 23:16:57 +0530

    oops 3 completed

--- python-assignment/oops_class3/test6.py
+++ python-assignment/oops_class3/test6.py
@@ -1,16 +1,23 @@
 class ineuron:
-    def student(self):
+    def students(self):
         print("The details of all students")
 
 
 class class_type:
-    def class_for_students(self):
+    def students(self):
         print("Print the class for student")
 
+def ineuron_external(a):
+    a.students()
+
+i=ineuron()
+j=class_type()
+ineuron_external(i)
+ineuron_external(j)
 
 def test(a, b):
     return a+b
 
 
-print(test(3, 5))
-print(test("flop ", "movie"))
+#print(test(3, 5))
+#print(test("flop ", "movie"))
