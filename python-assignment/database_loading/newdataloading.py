b8693843ee133a49e0a60dd41b85f2c0200ee73d
Pranshu Chaurasia <pranshuorian001@gmail.com>
2022-09-02 12:20:28 +0530

    Data Loading

--- python-assignment/database_loading/newdataloading.py
+++ python-assignment/database_loading/newdataloading.py
@@ -15,7 +15,7 @@ except Exception as Argument:
 data = pd.read_excel(r'C:\Users\Pranshu Chaurasia\Desktop\data-science-bootcamp\data-science-bootcamp\python-assignment\dataset\Dress Sales.xlsx')
 data.to_csv('C:\\Users\\Pranshu Chaurasia\\Desktop\\data-science-bootcamp\\data-science-bootcamp\\python-assignment\\dataset\\Dress Sales.csv',index=False)
 data1=data = pd.read_csv(r'C:\Users\Pranshu Chaurasia\Desktop\data-science-bootcamp\data-science-bootcamp\python-assignment\dataset\Dress Sales.csv',header=None)
-#print(data1)
+
 df = pd.DataFrame(data1)
 
 sql = "INSERT INTO dress_records.dress_dataset values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
@@ -38,6 +38,34 @@ for j in range (df.shape[0]):
                     l.append(int(float(temp_tup[i])))
 
         tup_new=tuple(l)
-        print(tup_new)
         mysql_conn.cursor.execute(sql,tup_new)
         mysql_conn.mydb.commit()
+
+cursor1 = mysql_conn.mydb.cursor()
+data2 = pd.read_excel(r'C:\Users\Pranshu Chaurasia\Desktop\data-science-bootcamp\data-science-bootcamp\python-assignment\dataset\Attribute DataSet.xlsx')
+data2.to_csv('C:\\Users\\Pranshu Chaurasia\\Desktop\\data-science-bootcamp\\data-science-bootcamp\\python-assignment\\dataset\\Attribute DataSet.csv',index=False)
+data3=data = pd.read_csv(r'C:\Users\Pranshu Chaurasia\Desktop\data-science-bootcamp\data-science-bootcamp\python-assignment\dataset\Attribute DataSet.csv')
+df = pd.DataFrame(data3)
+#df.isnull().sum()
+df=df.fillna(value='Not available')
+df.isnull().sum()
+sql = "INSERT INTO dress_records.attributes_dataset values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+for j in range (df.shape[0]):
+        temp_dict= dict(df.iloc[j,:])
+        temp_values=temp_dict.values()
+        temp_tup=tuple(temp_values)
+        l=[]
+        x=len(temp_tup)
+        for i in range (x):
+            if(i==0):
+                l.append(int(temp_tup[i]))
+            elif(i==3):
+                l.append(float(temp_tup[i]))
+            elif(i==13):
+                l.append(int(temp_tup[i]))
+            else:
+                l.append(str(temp_tup[i]))
+        tup_new=tuple(l)
+        cursor1.execute(sql,tup_new)
+        mysql_conn.mydb.commit()
+
